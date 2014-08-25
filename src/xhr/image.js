78c3949dc981259ab0032046199f5014df8111f0
Georg Ogris <georg.ogris@gmail.com>
2014-08-25 21:22:24 +0200

    working version for loading images via XHR

--- src/xhr/image.js
+++ src/xhr/image.js
@@ -2,32 +2,20 @@ import "../core/document";
 import "xhr";
 
 d3.image = function(url, callback) {
-  var xhr = d3_xhr(url, 'image/png', d3_image2, callback)
-      .responseType('arraybuffer');
-            //.responseType('blob');
+  var xhr = d3_xhr(url, 'image/png', d3_image, callback)
+      .responseType('blob');
+      //.responseType('arraybuffer');
   return callback == null ? xhr : xhr.get(d3_xhr_fixCallback(callback));
 };
 
-function d3_image(request) {
-  console.log(this.mimeType());
-  console.log(this.responseType());
-  console.log(request);
-  //var blob = new Blob([request.response], {type : 'image/png'});
-  //return blob;
-  //return new Uint8Array(request.response);
-  //return new Blob([request.response], {type : 'image/png'});
-  return request.response;
-}
-
-function d3_image2(request) {
-    var img = d3_document.createElement('img');
-    img.onload = function(e) {
-      console.log(e)
-      window.URL.revokeObjectURL(img.src); // Clean up after yourself.
-    };
-    var blob = new Blob([request.response], {type : 'image/png'}); // the blob 
-    img.src = d3_window.URL.createObjectURL(blob);
-    return img;
+function d3_image(request) { // TODO: fallback for old browser versions needed
+  var image = new Image();
+  image.onload = function(e) {
+      console.log("revoke callback for "+image.src)
+      d3_window.URL.revokeObjectURL(image.src); // TODO: wrap this to handle various browser versions
+  };
+  image.src = d3_window.URL.createObjectURL(request.response); // TODO: wrap this to handle various browser versions
+  return image;
 }
 
 d3.image2 = function(src, callback) {
@@ -36,36 +24,3 @@ d3.image2 = function(src, callback) {
   image.onerror = callback;
   image.onload = function() { callback(null, image); };
 };
-
-
-/*
-BlobBuilder = window.MozBlobBuilder || window.WebKitBlobBuilder || window.BlobBuilder;
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/path/to/image.png', true);
-xhr.responseType = 'arraybuffer';
-
-xhr.onload = function(e) {
-  if (this.status == 200) {
-    var bb = new BlobBuilder();
-    bb.append(this.response); // Note: not xhr.responseText
-
-    var blob = bb.getBlob('image/png');
-    var oMyBlob = new Blob(aFileParts, {type : 'text/html'}); // the blob
-    ...
-  }
-};
-
-xhr.send();
-
-
-var aFileParts = ['<a id="a"><b id="b">hey!</b></a>'];
-var oMyBlob = new Blob(aFileParts, {type : 'text/html'}); // the blob
-
-is equivalent to:
-
-var oBuilder = new BlobBuilder();
-var aFileParts = ['<a id="a"><b id="b">hey!</b></a>'];
-oBuilder.append(aFileParts[0]);
-var oMyBlob = oBuilder.getBlob('text/xml'); // the blob
-*/
